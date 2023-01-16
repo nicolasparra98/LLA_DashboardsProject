30d5ef6b2fe6f939deaf8e7f376b8d7a1e0d8874
nicolasparra98 <113445744+nicolasparra98@users.noreply.github.com>
2023-01-16 11:41:49 -0500

    Update 20230116_InitialChurnCheck.sql

--- Panama/20230116_InitialChurnCheck.sql
+++ Panama/20230116_InitialChurnCheck.sql
@@ -4,11 +4,12 @@ select  fixedmonth
         ,fixedchurntype
         ,fixedmainmovement
         ,b_techflag
+        ,F_ActiveBOM
         ,F_ActiveEOM
         ,count(distinct fixedaccount) as cuentas
         ,sum(B_NumRGUs) as BOM_RGUs
         ,sum(E_NumRGUs) as EOM_RGUs
 FROM FinalChurnFlag_SO
 WHERE fixedmonth = date('2023-01-01')
-GROUP BY 1,2,3,4,5
-ORDER BY 1,2,3,4,5
+GROUP BY 1,2,3,4,5,6
+ORDER BY 1,2,3,4,5,6
