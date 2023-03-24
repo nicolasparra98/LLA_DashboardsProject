f2e261d7616901bec626990c5d2b86d069f6d896
nicolasparra98 <113445744+nicolasparra98@users.noreply.github.com>
2023-03-24 12:07:54 -0500

    Update 202303236_GroupedFullFlags_v1.sql

--- Puerto Rico/Development Versions/202303236_GroupedFullFlags_v1.sql
+++ Puerto Rico/Development Versions/202303236_GroupedFullFlags_v1.sql
@@ -100,6 +100,7 @@ UNION ALL
 SELECT  *
 FROM "db_stage_dev"."lcpr_fmc_table_feb_mar23"
 )
+WHERE mob_s_att_duplicates = 1
 GROUP BY    1,2,3,4,5,6,7,8,9,10
             ,11,12,13,14,15,16,17,18,19,20
             ,21,22,23,24,25,26,27,28,29,30
